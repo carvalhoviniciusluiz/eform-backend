112cad798c9b16095f49534b7bbdd957d9f5afe3
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-11-03 00:38:17 -0300

    refactor: update app.module

--- src/accounts/presentation/accounts.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('accounts')
-export class AccountsController {
-  @Get()
-  getAll(): string {
-    return 'accounts';
-  }
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './ormconfig';
 import { RootModule } from './root/root.module';
-import { AccountsModule } from './accounts/accounts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), RootModule, AccountsModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), RootModule, UsersModule],
   exports: [TypeOrmModule]
 })
 export class AppModule {}

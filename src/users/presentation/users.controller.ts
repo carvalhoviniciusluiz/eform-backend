d10bf4f3b6844c811775a2eff41a7152a2aafdd7
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-11-04 13:49:14 -0300

    refactor: add user service

--- src/users/presentation/users.controller.ts
+++ src/users/presentation/users.controller.ts
@@ -1,37 +1,32 @@
 import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put, Query, ValidationPipe } from '@nestjs/common';
-import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { GetUsersCommand, CreateUserCommand, UpdateUserCommand } from 'users/application';
 import { CreateUserBodyDTO, UpdateUserBodyDTO } from 'users/presentation';
 import { RequestPaginateDto } from 'common/dtos';
 import { PaginatedUserResultsDto } from 'users/presentation/dtos';
-import { IUser } from 'users/domain';
+import { UserService } from 'users/domain';
 
 @ApiTags('USER')
 @Controller('users')
 export class UsersController {
-  constructor(readonly commandBus: CommandBus, readonly queryBus: QueryBus) {}
+  constructor(private readonly userService: UserService) {}
 
   @ApiOkResponse({ type: PaginatedUserResultsDto })
   @Get()
-  async getAll(@Query(ValidationPipe) paginateDto: RequestPaginateDto): Promise<PaginatedUserResultsDto> {
+  async find(@Query(ValidationPipe) paginateDto: RequestPaginateDto): Promise<PaginatedUserResultsDto> {
     const { page, pagesize } = paginateDto;
-    const command = new GetUsersCommand(page, pagesize);
-    const [rows, count] = await this.commandBus.execute<GetUsersCommand, [IUser[], number]>(command);
+    const [rows, count] = await this.userService.find(page, pagesize);
     return new PaginatedUserResultsDto(rows, count, page, pagesize);
   }
 
   @ApiResponse({ status: 200, description: 'Success' })
   @Post()
   async store(@Body() body: CreateUserBodyDTO): Promise<void> {
-    const command = new CreateUserCommand(body);
-    await this.commandBus.execute(command);
+    await this.userService.save(body);
   }
 
   @ApiResponse({ status: 200, description: 'Success' })
   @Put('/:id')
-  update(@Body() body: UpdateUserBodyDTO, @Param('id', new ParseUUIDPipe()) id: string): void {
-    const command = new UpdateUserCommand(id, body);
-    this.commandBus.execute(command);
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: UpdateUserBodyDTO): Promise<void> {
+    await this.userService.update(id, body);
   }
 }

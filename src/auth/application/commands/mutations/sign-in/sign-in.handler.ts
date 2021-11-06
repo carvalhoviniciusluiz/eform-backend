b167b70ec9a2b0fe9fe9f91f108cf626f9028ba4
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-11-06 13:01:25 -0300

    refactor: add return to token.exp

--- src/auth/application/commands/mutations/sign-in/sign-in.handler.ts
+++ src/auth/application/commands/mutations/sign-in/sign-in.handler.ts
@@ -4,6 +4,10 @@ import { SignInCommand } from 'auth/application/commands/mutations/sign-in';
 import { SignInProps } from 'auth/domain';
 import * as ENV from 'auth/../constants';
 
+type DecodedProps = {
+  exp: number;
+};
+
 @CommandHandler(SignInCommand)
 export class SignInHandler implements ICommandHandler<SignInCommand, SignInProps> {
   constructor(private jwtService: JwtService) {}
@@ -11,6 +15,8 @@ export class SignInHandler implements ICommandHandler<SignInCommand, SignInProps
   async execute(command: SignInCommand): Promise<SignInProps> {
     const { payload } = command;
     const accessToken = this.jwtService.sign(payload);
+    const accessTokenDecoded = this.jwtService.decode(accessToken) as DecodedProps;
+    const accessTokenExpiresIn = accessTokenDecoded.exp;
 
     const refreshTokenOptions: JwtSignOptions = {
       expiresIn: ENV.JWT_SECRET_REFRESHTOKEN_EXPIRES_IN,
@@ -18,10 +24,15 @@ export class SignInHandler implements ICommandHandler<SignInCommand, SignInProps
     };
 
     const refreshToken = this.jwtService.sign(payload, refreshTokenOptions);
+    const refreshTokenDecoded = this.jwtService.decode(refreshToken) as DecodedProps;
+    const refreshTokenExpiresIn = refreshTokenDecoded.exp;
 
     return {
       accessToken,
-      refreshToken
+      accessTokenExpiresIn,
+      refreshToken,
+      refreshTokenExpiresIn,
+      tokenType: 'bearer'
     };
   }
 }
--- src/auth/domain/protocols/sign-in.type.ts
+++ src/auth/domain/protocols/sign-in.type.ts
@@ -1,4 +1,7 @@
 export type SignInProps = {
   accessToken: string;
+  accessTokenExpiresIn: number;
   refreshToken: string;
+  refreshTokenExpiresIn: number;
+  tokenType: string;
 };

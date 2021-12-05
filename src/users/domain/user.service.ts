3be037b6507bb98da652a9667e9375c5afe73e86
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-05 13:55:00 -0300

    refactor: add new variables

--- src/users/domain/user.service.ts
+++ src/users/domain/user.service.ts
@@ -10,15 +10,17 @@ export class UserService {
   constructor(readonly commandBus: CommandBus, readonly queryBus: QueryBus) {}
 
   async find(page: number, pagesize: number): Promise<[IUser[], number]> {
-    const command = new FindUsersQuery(page, pagesize);
-    return this.queryBus.execute<FindUsersQuery, [IUser[], number]>(command);
+    const query = new FindUsersQuery(page, pagesize);
+    return this.queryBus.execute<FindUsersQuery, [IUser[], number]>(query);
   }
 
   async save(props: TUser): Promise<void> {
-    this.commandBus.execute(new CreateUserCommand(props));
+    const command = new CreateUserCommand(props);
+    this.commandBus.execute(command);
   }
 
   async update(id: string, props: TUserWithoutPassword): Promise<void> {
-    this.commandBus.execute(new UpdateUserCommand(id, props));
+    const command = new UpdateUserCommand(id, props);
+    this.commandBus.execute(command);
   }
 }
